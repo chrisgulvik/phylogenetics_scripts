451ee3a1d28bde778a662f494ff283a45a4fe08e
Christopher Gulvik <cgulvik@gmail.com>
2016-05-06 17:34:20 -0400

    big speedup (>50%) and now handles cases where input lacks any [ATCG-N]

--- remove_uninformative_sites.py
+++ remove_uninformative_sites.py
@@ -28,57 +28,45 @@ def get_variant_sites(infile):
 		alignment = MultipleSeqAlignment([rec.upper() for rec in aln],
 			annotations=aln.annotations)
 		summary_align = AlignInfo.SummaryInfo(alignment)
-		sequence = (alignment[0].seq)  #arbitrarily chose 1st seq to compare all others to
+		sequence = alignment[0].seq  #arbitrarily chose 1st seq to compare all others to
 
 		pssm_sequence = summary_align.pos_specific_score_matrix(sequence)#, chars_to_ignore=['N'])
 		#'pssm_sequence' is an obj containing line-by-line [site_identity, -, A, C, G, T]
-		count = 0
+		site_index = 0
 		num_invariants = 0
-		invariant_position_index = []
+		variant_position_index = []
 
+		num_records = len(alignment)
+		print 'Number of records in multifasta: {}'.format(num_records)
 		for site in pssm_sequence.pssm:
-			print site
+			sys.stdout.write('.')
 			if site[0] != '-' and site[1]['-'] == 0:
-				#site is a tuple of site_identity and dict of corresponding PSSM keys=nucleotides, vals=scores
-				gap_score = site[1]['-']
-				A_score   = site[1]['A']
-				C_score   = site[1]['C']
-				G_score   = site[1]['G']
-				T_score   = site[1]['T']
-				N_score   = site[1]['N']
-				site_scores = [gap_score, A_score, C_score, G_score, T_score, N_score]  #same order as pssm obj 'pssm_sequence'
-				binary_index = []
-				for score in site_scores:
-					if score > 0:
-						binary_index.append(1)  #simplify with binary (present)
-					else:
-						binary_index.append(0)  #simplify with binary (absent)
-				if sum(binary_index[1:len(binary_index)]) > 1:
-					pass
-				elif sum(binary_index[1:len(binary_index)]) == 1:
+			#site is a tuple of site_identity and dict of corresponding PSSM keys=nucleotides, vals=scores
+				site_pssm_freqs = []
+				for nucleotide in site[1]:
+					site_pssm_freqs.append(int(str(site[1][nucleotide]).split('.')[0]))
+				
+				if num_records in site_pssm_freqs:
 					num_invariants += 1
-					invariant_position_index.append(count)
+				elif num_records not in site_pssm_freqs:
+					variant_position_index.append(site_index)
 				else:
 					sys.exit('ERROR_1')
-				count += 1
-			elif site[0] == '-' or site[1]['-'] > 0:
+				site_index += 1
+
+			elif site[0] == '-' or site[1]['-'] > 0:  #skip sites with any number of gaps
 				num_invariants += 1
-				invariant_position_index.append(count)
-				count += 1
+				site_index += 1
 			else:
 				sys.exit('ERROR_2')
 
-	variant_position_index = []
-	for i in range(0, len(sequence)):
-		if i not in invariant_position_index:
-			variant_position_index.append(i)
-
+	print '\nFound {} variant sites without gaps'.format(str(len(variant_position_index)))
 	print 'Discarded {} invariant and gap-containing sites'.format(
-		str(len(invariant_position_index)))
+		str(num_invariants))
 	return variant_position_index, alignment
 
 def ranges(i):
-	for (a, b) in itertools.groupby(enumerate(i), lambda (x, y): y - x):
+	for (a, b) in itertools.groupby(enumerate(i), lambda (x, y): y-x):
 		b = list(b)
 		yield b[0][1], b[-1][1]
 
@@ -86,15 +74,21 @@ def write_variant_sites(alignment, var_sites, outfile):
 	nex_aligns = []  #Bio.Nexus.Nexus.Nexus objects
 	blocks = list(ranges(var_sites))  #tuples of positions
 	for i in blocks:
-		alignment_iteration = MultipleSeqAlignment(alignment[:, i[0]:i[1] + 1],
+		alignment_iteration = MultipleSeqAlignment(alignment[:, i[0]:i[1]+1],
 			alphabet=generic_dna).format('nexus')
-		nex_aligns.append(('site {} to {}'.format(str(i[0]), str(i[1] + 1)),
+		# if i[0] == i[1]:
+		# 	nex_aligns.append(('site {}'.format(str(i[1] + 1)),
+		# 		Nexus.Nexus(alignment_iteration)))
+		# else:
+		# 	nex_aligns.append(('site {} to {}'.format(str(i[0]), str(i[1] + 1)),
+		# 		Nexus.Nexus(alignment_iteration)))
+		nex_aligns.append(('site {} to {}'.format(str(i[0]), str(i[1]+1)),
 			Nexus.Nexus(alignment_iteration)))
 
 	combined = Nexus.combine(nex_aligns)
 	with open(outfile, 'w') as out:
 		combined.write_nexus_data(out)
-	print 'Converted {} informative sites into nexus alignment'.format(str(len(blocks)))
+	print 'Converted {} informative sites without gaps into nexus alignment'.format(str(len(blocks)))
 
 def main():
 	opts = parseArgs()
